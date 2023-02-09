a08f7fcc87a1c5bdf682d361e9fa073b56c5dcc8
Vivek <tailorvivekgec19@gmail.com>
2023-02-08 21:24:15 -0800

    new

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -1,3 +1,5 @@
+//stful = setState live update
+//stless = no live update
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -8,6 +10,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int i = 1;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,13 +18,24 @@ class _HomeScreenState extends State<HomeScreen> {
         title: Text(
           "Counter Application",
           style: TextStyle(
-              color: Colors.redAccent,
+              color: Colors.white,
               fontWeight: FontWeight.bold,
-              fontSize: 30),
+              fontSize: 20),
         ),
         centerTitle: true,
+        actions: [
+          GestureDetector(
+            child: Icon(Icons.add_circle_outline),
+            onTap: (){
+              setState(() { //Live updates
+                i++;
+              });
+            },
+          ),
+
+
+        ],
       ),
-      body: ,
     );
   }
 }
